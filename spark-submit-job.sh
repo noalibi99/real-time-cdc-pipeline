5e31b0f6be16d9ea2eff7469a07e2a570a90c9f2
amineedarkaoui <amineed98@gmail.com>
2026-05-21 07:25:37 +0000

    bronze on iceberg

--- spark-submit-job.sh
+++ spark-submit-job.sh
@@ -7,6 +7,8 @@ fi
 JOB=$1
 shift
 
+JOB_DIR="/home/iceberg/notebooks/notebooks"
+
 PACKAGES="org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.5,\
 org.apache.kafka:kafka-clients:3.4.1,\
 org.apache.spark:spark-token-provider-kafka-0-10_2.12:3.5.5,\
@@ -14,8 +16,8 @@ org.apache.commons:commons-pool2:2.11.1,\
 org.apache.hadoop:hadoop-aws:3.3.4,\
 com.amazonaws:aws-java-sdk-bundle:1.12.367"
 
-docker exec spark-master /opt/spark/bin/spark-submit \
-  --master spark://spark-master:7077 \
+docker exec spark-iceberg /opt/spark/bin/spark-submit \
+  --master local[*] \
   --executor-memory 1g \
   --executor-cores 2 \
   --total-executor-cores 2 \
@@ -23,4 +25,4 @@ docker exec spark-master /opt/spark/bin/spark-submit \
   --conf spark.driver.extraJavaOptions="-Divy.home=/tmp/.ivy2" \
   --packages "$PACKAGES" \
   "$@" \
-  /opt/spark/jobs/"$JOB"
+  "$JOB_DIR/$JOB"
